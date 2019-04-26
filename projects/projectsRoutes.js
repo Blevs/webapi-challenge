41c9c2bf44ad2f3ba554af1c5047d5577f8ac536
Henry Blevins <heblevi@gmail.com>
2019-04-26 11:40:43 -0400

    Implement creating projects

--- projects/projectsRoutes.js
+++ projects/projectsRoutes.js
@@ -1,17 +1,29 @@
 const express = require('express');
-const project = require('../data/helpers/projectModel.js');
+const projectModel = require('../data/helpers/projectModel.js');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  project.get()
+  projectModel.get()
     .then(projects => res.status(200).json(projects))
     .catch(_err => res.status(500).json({message: "Error getting project information"}));
 });
 
+router.post('/', (req, res) => {
+  const project = req.body;
+  if (project && project.name && project.description) {
+    project.completed = project.completed || false;
+    projectModel.insert(project)
+      .then(project => res.status(201).json(project))
+      .catch(_err => res.status(500).json({message: "Error creating project"}));
+  } else {
+    res.status(400).json({message: "Provide project name and description"});
+  }
+});
+
 router.get('/:id', (req, res) => {
   const {id} = req.params;
-  project.get(id)
+  projectModel.get(id)
     .then(project => project
           ? res.status(200).json(project)
           : res.status(404).json({message: "Project with give ID does not exist."}))
