929644ad826b265aca42220aa9bf86832da9c591
Henry Blevins <heblevi@gmail.com>
2019-04-26 11:33:33 -0400

    Implement get project by id

--- projects/projectsRoutes.js
+++ projects/projectsRoutes.js
@@ -9,4 +9,13 @@ router.get('/', (req, res) => {
     .catch(_err => res.status(500).json({message: "Error getting project information"}));
 });
 
+router.get('/:id', (req, res) => {
+  const {id} = req.params;
+  project.get(id)
+    .then(project => project
+          ? res.status(200).json(project)
+          : res.status(404).json({message: "Project with give ID does not exist."}))
+    .catch(_err => res.status(500).json({message: "Error getting project information"}));
+});
+
 module.exports = router;
