e00edda7615c9e4e426e11678a1c696a3fd207b6
Henry Blevins <heblevi@gmail.com>
2019-04-26 12:05:03 -0400

    Implement creating new actions

--- actions/actionsRoutes.js
+++ actions/actionsRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const actionModel = require('../data/helpers/actionModel.js');
+const projectModel = require('../data/helpers/projectModel.js');
 
 const router = express.Router();
 
@@ -7,6 +8,26 @@ router.get('/', (req, res) => {
   actionModel.get()
     .then(actions => res.status(200).json(actions))
     .catch(_err => res.status(500).json({message: "Error getting actions"}));
+});
+
+router.post('/', (req, res) => {
+  const action = req.body;
+  if (action && action.project_id && action.description && action.notes) {
+    if (action.description.length > 128) {
+      res.status(400).json({message: "Action description must be under 128 characters"});
+    } else {
+    action.completed = action.completed || false;
+    projectModel.get(action.project_id)
+      .then(project => project
+            ? actionModel.insert(action)
+            .then(action => res.status(200).json(action))
+            .catch(_err => res.status(500).json({message: "Error creating action"}))
+            : res.status(400).json({message: "Project with ID does not exist"}))
+      .catch(_err => res.status(500).json({message: "Error getting project"}));
+    }
+  } else {
+    res.status(400).json({message: "Provide action project_id, description and notes"});
+  }
 });
 
 module.exports = router;
